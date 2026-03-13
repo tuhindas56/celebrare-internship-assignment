20cce039b971630968c74d0267d1bf9e6ac16594
tuhindas56 <67787961+tuhindas56@users.noreply.github.com>
2026-03-13 18:51:05 +0000

    feat: implement header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Gallery from "@/components/gallery"
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="mx-auto flex max-w-7xl flex-col gap-8 p-4">
+      <header className="border-border flex w-full flex-wrap items-center justify-between rounded-md border px-4 py-3">
+        <div>
+          <h1 className="text-lg font-medium">Photo Gallery</h1>
+          <p className="text-xs">assignment by Tuhin Das</p>
+        </div>
+        <a
+          href="https://github.com/tuhindas56/celebrare-internship-assignment"
+          target="_blank"
+          className="text-sm text-blue-500 hover:underline"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App
